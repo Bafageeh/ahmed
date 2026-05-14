8ad80578570eee2c6cdc3f9621131e1e3630f525
Bafageeh <a.baf@live.com>
2026-05-15 00:09:28 +0300

    Attach selected Ahmed account to mobile API requests

--- ahmed-mobile/index.js
+++ ahmed-mobile/index.js
@@ -1,3 +1,11 @@
 import { registerRootComponent } from 'expo';
 import App from './AppShell';
+import { ahmedUserHeaders } from './ahmedCurrentUser';
+
+const baseFetch = globalThis.fetch;
+globalThis.fetch = (input, init = {}) => {
+  const headers = ahmedUserHeaders(init.headers || {});
+  return baseFetch(input, { ...init, headers });
+};
+
 registerRootComponent(App);
