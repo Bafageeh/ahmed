2b9922ddb6fa844efd69d9226f6995c9a7bbdfa5
Bafageeh <a.baf@live.com>
2026-07-10 21:45:26 +0300

    Schedule monthly government support application

--- ahmed-api/routes/console.php
+++ ahmed-api/routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Services\DebtAutoPaymentService;
+use App\Services\DebtGovernmentSupportService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -14,5 +15,13 @@ Schedule::call(function () {
 })
     ->name('debts:auto-pay')
     ->dailyAt('00:05')
+    ->timezone('Asia/Riyadh')
+    ->withoutOverlapping();
+
+Schedule::call(function () {
+    app(DebtGovernmentSupportService::class)->apply();
+})
+    ->name('debts:government-support')
+    ->dailyAt('00:10')
     ->timezone('Asia/Riyadh')
     ->withoutOverlapping();
