25eacbed0df39ef987bad32d695c21b8b4549fe1
Bafageeh <a.baf@live.com>
2026-07-10 18:13:21 +0300

    Schedule daily automatic debt payments

--- ahmed-api/routes/console.php
+++ ahmed-api/routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Services\DebtAutoPaymentService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    app(DebtAutoPaymentService::class)->apply();
+})
+    ->name('debts:auto-pay')
+    ->dailyAt('00:05')
+    ->timezone('Asia/Riyadh')
+    ->withoutOverlapping();
