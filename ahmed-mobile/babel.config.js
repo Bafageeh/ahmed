e6ac96b5438664bf4483209f2a46d137a266a2d3
Bafageeh <a.baf@live.com>
2026-08-19 23:58:11 +0300

    Enable Mercedes financing analysis cards

--- ahmed-mobile/babel.config.js
+++ ahmed-mobile/babel.config.js
@@ -8,6 +8,7 @@ module.exports = function (api) {
       './babel-plugin-debt-detail-extra-stats',
       './babel-plugin-debt-header-safe-area',
       './babel-plugin-debt-last-payment-stat',
+      './babel-plugin-mercedes-finance-analysis',
     ],
   };
 };
