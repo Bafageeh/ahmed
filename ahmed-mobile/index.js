745dbb43b85773e9397d8d1150c84a5387d74202
Bafageeh <a.baf@live.com>
2026-08-22 14:01:25 +0300

    Run Ahmed auto-updater at app startup

--- ahmed-mobile/index.js
+++ ahmed-mobile/index.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import App from './AppShellWithAccountSelector';
+import AutoUpdater from './AutoUpdater';
 import { ahmedUserHeaders } from './ahmedCurrentUser';
 
 const baseFetch = globalThis.fetch;
@@ -8,4 +10,8 @@ globalThis.fetch = (input, init = {}) => {
   return baseFetch(input, { ...init, headers });
 };
 
-registerRootComponent(App);
+function Root() {
+  return React.createElement(AutoUpdater, null, React.createElement(App));
+}
+
+registerRootComponent(Root);
