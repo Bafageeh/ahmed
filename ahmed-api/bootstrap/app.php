75589f59873bc9cad30eff291cc5a41fc5efc53e
Bafageeh <a.baf@live.com>
2026-08-30 08:10:43 +0300

    Allow Mada vault cards without statement date

--- ahmed-api/bootstrap/app.php
+++ ahmed-api/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AhmedAuthenticate;
+use App\Http\Middleware\NormalizeMadaVaultCard;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../app/Console/Commands',
     ])
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(NormalizeMadaVaultCard::class);
         $middleware->alias([
             'ahmed.auth' => AhmedAuthenticate::class,
         ]);
