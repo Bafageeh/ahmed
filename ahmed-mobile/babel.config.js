b33e855e9d39a479cee5765de04e88e16bd4eccc
Bafageeh <a.baf@live.com>
2026-09-05 12:10:17 +0300

    تفعيل ظهور رقم السداد للبطائق الائتمانية

--- ahmed-mobile/babel.config.js
+++ ahmed-mobile/babel.config.js
@@ -18,6 +18,7 @@ module.exports = function (api) {
       './babel-plugin-secure-vault-sadad-cvv',
       './babel-plugin-secure-vault-iban-single-line',
       './babel-plugin-secure-vault-national-id-info',
+      './babel-plugin-secure-vault-sadad-form-visible',
     ],
   };
 };
