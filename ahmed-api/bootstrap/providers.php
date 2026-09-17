f7a8df596abe62b1c8b3a118694475422136012a
Bafageeh <a.baf@live.com>
2026-09-17 20:40:01 +0300

    Register Ta3meed Excel export provider

--- ahmed-api/bootstrap/providers.php
+++ ahmed-api/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\Ta3meedExcelExportServiceProvider;
 
 return [
     AppServiceProvider::class,
+    Ta3meedExcelExportServiceProvider::class,
 ];
